fdf13f58ed3ee14ef9594450baec4c584c59daa5
hirotaka_maki <h.maki@laniakea-tech.com>
2025-12-09 07:03:57 +0900

    fix

--- backend/config/middleware.py
+++ backend/config/middleware.py
@@ -1,9 +1,16 @@
-# config/middleware.py
+# config/cors_middleware.py
 from django.http import HttpResponse
 
-def SimpleCorsDebugMiddleware(get_response):
+
+def SimpleCORSMiddleware(get_response):
+    """
+    非常用の簡易 CORS ミドルウェア。
+    全レスポンスに CORS ヘッダを付ける。
+    プロダクション用というより、まずは PoC 用。
+    """
+
     def middleware(request):
-        # OPTIONS の場合は空レスポンスでもOK
+        # OPTIONS の場合はプリフライト用に空レスポンスでもOK
         if request.method == "OPTIONS":
             response = HttpResponse()
         else:
@@ -11,11 +18,21 @@ def SimpleCorsDebugMiddleware(get_response):
 
         origin = request.headers.get("Origin")
         if origin:
-            # 今回はフロントからの Origin をそのまま返す
+            # 今回は来た Origin をそのまま返す（フロントが1つなのでOK）
             response["Access-Control-Allow-Origin"] = origin
+            response["Vary"] = "Origin"
 
+        # 許可メソッド
         response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-        response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+
+        # 許可ヘッダ（プリフライトで要求されているヘッダをそのまま返す）
+        request_headers = request.headers.get("Access-Control-Request-Headers")
+        if request_headers:
+            response["Access-Control-Allow-Headers"] = request_headers
+        else:
+            response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+
+        # 認証情報を含める場合
         response["Access-Control-Allow-Credentials"] = "true"
 
         return response
